f03c9aeafabfee681cf2e2a646816da0fba9ea1b
yyy-01 <78523775+yyy-01@users.noreply.github.com>
2021-02-10 23:58:42 +0800

    Add files via upload

---
+++ Pet.java
@@ -0,0 +1,15 @@
+package com.mashibing;
+
+/**
+ * @Auther:yyy
+ * @Date:2021/2/10 -02  -17:35
+ * @Descriptionn:com.mashibing
+ * @version:1.0
+ */
+public abstract class Pet {
+public abstract void feed();
+
+    public void  play() {
+        System.out.println("play.....");
+    }
+}
